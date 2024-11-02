d41dab01b6207c3286e62f1a1d6a0b009117c333
Yiiong <1783172311@qq.com>
2024-11-02 23:59:20 +0800

    fix: 修复了单击一次BAN按钮后状态与实际操作不一致的问题

--- pkgs/data/app_current.py
+++ pkgs/data/app_current.py
@@ -1,9 +1,12 @@
-
-from flask import Flask, jsonify
+import logging
+from flask import Flask, jsonify, request
 import subprocess
 import datetime
 
 app = Flask(__name__)
+
+# log
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s',  handlers=[logging.StreamHandler()])
 
 @app.route('/execute', methods=['GET'])
 def execute_command():
@@ -34,28 +37,33 @@ def ban_ips():
     # 从请求的JSON主体中获取IP列表
     ip_data_list = request.json
     if not ip_data_list:
+        logging.warning("Missing IP data in request body!")
         return jsonify({"error": "Missing IP data in request body"}), 400
 
     results = []
     for ip_data in ip_data_list:
         ip = ip_data.get("ip")
         count = ip_data.get("count", 0)
-        if count > 250:
+        if count >= 250:
             try:
                 # 对每个IP执行curl命令
-                curl_command = f"curl -s '198.18.114.2:8080/update?cidr={ip}&ban_type=0&ban_time=360000'"
+                curl_command = f"curl -s '198.18.114.2:8080/update?cidr={ip}&ban_type=0&ban_time=2592000'"
                 curl_output = subprocess.check_output(curl_command, shell=True, text=True)
                 # 检查curl命令的输出
-                if f"{ip} have been updated" in curl_output:
+                if f"Successfully added {ip} to banned list" in curl_output or f'{ip} have been updated' in curl_output:
+                    logging.info(f"Succeed to ban {ip}")
                     results.append({"ip": ip, "count": count, "status": "success"})
                 else:
+                    # 添加日志
+                    logging.error(f"Failed to ban {ip}: cur_output: {curl_output}")
                     results.append({"ip": ip, "count": count, "status": "failed"})
             except subprocess.CalledProcessError as e:
+                logging.error(f"Failed to ban {ip}: {e}")
                 results.append({"ip": ip, "count": count, "status": "failed", "error": str(e)})
         else:
+            logging.info(f"Skipped IP: {ip}, count: {count}")
             results.append({"ip": ip, "count": count, "status": "skipped"})
 
     return jsonify(results)
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9521)
-
+    app.run(host='172.20.14.2', port=9521)
